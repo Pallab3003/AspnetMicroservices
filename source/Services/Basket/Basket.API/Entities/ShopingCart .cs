1fc13693d3851a14c051f138c7d20e6da002e1fd
pallab1988 <sarpal3003@gmail.com>
2022-01-27 14:12:46 +0530

    27/01/2022

---
+++ source/Services/Basket/Basket.API/Entities/ShopingCart .cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShopingCart
+    {
+   
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items { get; set; } = new List<ShoppingCartItems>();
+
+        public ShopingCart()
+        {
+        }
+        public ShopingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get 
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+
+            
+        }
+
+    }
+}
